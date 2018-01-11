f158a5f4a7f140dd3686c6eb0b7ebefada366780
Civerooni <civerooni@gmail.com>
2018-01-11 16:45:08 -0700

    Update control.lua
    Changing force.name call to be just force for ceasefire command

--- BiterAggroControl_0.1.0/control.lua
+++ BiterAggroControl_0.1.0/control.lua
@@ -2,13 +2,13 @@
 
 script.on_event({defines.events.on_player_joined_game}, 
     function(event)
-        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force.name, false)
+        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, false)
     end
 )
 
 script.on_event({defines.events.on_player_left_game}, 
     function(event)
-        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force.name, true)
+        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, true)
     end
 )
 	
