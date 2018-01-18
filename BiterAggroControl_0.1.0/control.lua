a755e208750759afe13989bd70c2e7bd955f0c41
Civerooni <civerooni@gmail.com>
2018-01-18 14:33:25 -0700

    Update control.lua
    Added comments

--- BiterAggroControl_0.1.0/control.lua
+++ BiterAggroControl_0.1.0/control.lua
@@ -1,17 +1,20 @@
 --control.lua
 
+--Set cease fire to false on player join
 script.on_event({defines.events.on_player_joined_game}, 
     function(event)
         game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, false)
     end
 )
 
+--Set cease fire to true on player leave
 script.on_event({defines.events.on_player_left_game}, 
     function(event)
         game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, true)
     end
 )
 	
+--Set cease fire to all disconnected players forces
 script.on_event({defines.events.on_tick},
    function (event)
       if event.tick % 600 == 0 then --reduce how often this runs, every 10 seconds
