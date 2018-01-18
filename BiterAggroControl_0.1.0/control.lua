76c35eef4f8d2bff586cc3c3716106b3988a9207
Civerooni <civerooni@gmail.com>
2018-01-18 12:45:25 -0700

    Update control.lua
    Adding check on tick for all players on all forces to see if they are connected or not. If they are not they will be set in ceasefire with the enemy.

--- BiterAggroControl_0.1.0/control.lua
+++ BiterAggroControl_0.1.0/control.lua
@@ -12,5 +12,22 @@ script.on_event({defines.events.on_player_left_game},
     end
 )
 	
-
-
+script.on_event({defines.events.on_tick},
+   function (event)
+      if event.tick % 600 == 0 then --reduce how often this runs, every 10 seconds
+	local player_is_connected = false
+         -- Set all forces to friendly
+	for name, force in pairs (game.forces) do
+	   for player_name, player in pairs (force.players) do
+	      for connected_player_name, connected_player in pairs (force.connected_players) do
+		 if player_name == connected_player_name then
+		    player_is_connected = true
+		 end
+	      end
+	      -- Set enemy cease fire state to opposite of the connected state
+	      game.forces["enemy"].set_cease_fire(force, not player_is_connected)
+	   end
+	end
+      end
+   end
+)
