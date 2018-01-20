f066adaa3b176ba6fb71a3c858a7446dd29b5cff
Civerooni <civerooni@gmail.com>
2018-01-19 17:36:19 -0700

    Update control.lua
    Changing cease fire to be mega turrets

--- BiterAggroControl_0.1.0/control.lua
+++ BiterAggroControl_0.1.0/control.lua
@@ -3,21 +3,23 @@
 --Set cease fire to false on player join
 script.on_event({defines.events.on_player_joined_game}, 
     function(event)
-        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, false)
+        --game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, false)
+	game.players[event.player_index].force.set_turret_attack_modifier("turret", 1)
     end
 )
 
 --Set cease fire to true on player leave
 script.on_event({defines.events.on_player_left_game}, 
     function(event)
-        game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, true)
+        --game.forces["enemy"].set_cease_fire(game.players[event.player_index].force, true)
+	game.players[event.player_index].force.set_turret_attack_modifier("turret", 1000)
     end
 )
 	
 --Set cease fire to all disconnected players forces
 script.on_event({defines.events.on_tick},
    function (event)
-      if event.tick % 600 == 0 then --reduce how often this runs, every 10 seconds
+      if event.tick % 60 == 0 then --reduce how often this runs, every 1 second
 	local player_is_connected = false
          -- Set all forces to friendly
 	for name, force in pairs (game.forces) do
@@ -28,7 +30,11 @@ script.on_event({defines.events.on_tick},
 		 end
 	      end
 	      -- Set enemy cease fire state to opposite of the connected state
-	      game.forces["enemy"].set_cease_fire(force, not player_is_connected)
+	      if player_is_connected == true then
+		 game.players[event.player_index].force.set_turret_attack_modifier("turret", 1)
+	      else
+		 game.players[event.player_index].force.set_turret_attack_modifier("turret", 1000)
+	      end
 	      player_is_connected = false
 	   end
 	end
